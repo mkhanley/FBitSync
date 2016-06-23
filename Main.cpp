0f097e5e4ca73820e725fbfc11bac644cd8ccd54
Mark <mkhanley1@gmail.com>
2016-06-23 22:49:06 +0100

    Retrive Dongle info

--- Main.cpp
+++ Main.cpp
@@ -13,12 +13,11 @@ int main() {
     libusb_device **list;
     ssize_t count = libusb_get_device_list(NULL, &list);
     ssize_t i = 0;
-    int err = 0;
+    int res = 0;
     if (count < 0) {
-        cout << "None found";
+        cout << "No USB devices found";
         exit(-1);
     }
-    printf("\n%d\n", (int)count);
     libusb_device_handle *handle;
     handle = libusb_open_device_with_vid_pid(NULL, venID, prodID);
     if(handle == NULL)
@@ -31,6 +30,28 @@ int main() {
         if(libusb_detach_kernel_driver(handle, 0) == 0) //detach it
             cout<<"Kernel Driver Detached!"<<endl;
     }
+    else
+        cout << "Kernel Driver not active" << endl;
+    res = libusb_claim_interface(handle, 0);
+    if(res < 0 ){
+        cout << "Cannot claim interface" <<endl;
+        return 1;
+    }
+    else
+        cout << "Interface claimed" <<  endl;
+    uint8_t wdata[3], *rdata;
+    wdata[0] = 0x02;
+    wdata[1] = 0x01;
+    rdata = (uint8_t *)calloc(32, 1);
+    int actual;
+    res = libusb_bulk_transfer(handle,0x02, wdata, 3, &actual, 2000);
+    if(res == 0 && actual == 32) //we wrote the 4 bytes successfully
+    cout<<"Writing Successful!"<<endl;
+    else
+    cout<<"Write Error"<<endl;
+    res = libusb_bulk_transfer(handle, 0x82u, rdata, 32, &actual,000);
+    if(res == 0 && actual > 0)
+        cout<< "Read" <<endl;
 
     return 0;
 }
